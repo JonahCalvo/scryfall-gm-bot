de878fa605246e1e80e87c5cdfec8825585ac2d6
Jonah Calvo <calvo013@umn.edu>
2022-02-07 18:00:33 -0800

    testing

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 var HTTPS = require('https');
 var cool = require('cool-ascii-faces');
 var scryfall = require("scryfall-client");
-const fetch = require('node-fetch');
+import fetch from "node-fetch";
 
 var botID = process.env.BOT_ID;
 
