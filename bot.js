8fe3cbcebe4e6971542ceb450795f6184cc3c4a1
Jonah Calvo <calvo013@umn.edu>
2022-02-07 18:08:43 -0800

    testing

--- bot.js
+++ bot.js
@@ -30,7 +30,7 @@ function postMessage(cardName) {
     image = card.getImage();
 
     fetch(image)
-      .then(r => console.log(r.blob));
+      .then(r => console.log(r.blob()));
 
     botResponse = card.name;
 
